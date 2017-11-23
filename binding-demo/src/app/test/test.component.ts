c44faa647c657e8b1b53b03520bb96656ac9efb3
Vishwas <gopinav5@gmail.com>
2017-11-23 14:13:28 +0530

    Add Interpolation and Property Binding

--- binding-demo/src/app/test/test.component.ts
+++ binding-demo/src/app/test/test.component.ts
@@ -3,17 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <h2>
-      Welcome Vishwas
-    </h2>
+    <div>
+      <h2>Welcome {{name}}</h2>
+      <h2>{{2+2}}</h2>
+      <h2>{{"Welcome " + name}}</h2>
+      <h2>{{name.length}}</h2>
+      <h2>{{name.toUpperCase()}}</h2>
+      <h2>{{greetUser()}}</h2>
+      <h2>{{url}}</h2>
+
+      <input [id]="myId" bind-disabled="isDisabled"  type="text" value="Vishwas">
+      <input id="{{myId}}" [disabled]="isDisabled"  type="text" value="Vishwas">
+      
+    </div>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
+  public name = "Codevolution";
+  public url = window.location.href;
+  public myId = "testId";
+  public isDisabled = true;
   constructor() { }
 
   ngOnInit() {
   }
+
+  greetUser(){
+    return "Hello " + this.name;
+  }
+
+  
 
 }
