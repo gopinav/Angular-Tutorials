14855e998c97418f5e98e87a77eb47a6415dbfe7
Vishwas <gopinav5@gmail.com>
2017-11-23 09:52:39 +0530

    Add test component

--- binding-demo/src/app/app.module.ts
+++ binding-demo/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
---
+++ binding-demo/src/app/test/test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <h2>
+      Welcome Vishwas
+    </h2>
+  `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
