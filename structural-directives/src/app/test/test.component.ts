80d7613ae509b47f462a2cf28f6373850719c9e0
Vishwas <gopinav5@gmail.com>
2017-11-26 21:44:21 +0530

    ngIf Directive

--- structural-directives/src/app/test/test.component.ts
+++ structural-directives/src/app/test/test.component.ts
@@ -3,14 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-test',
   template: `
-    <p>
-      test works!
-    </p>
+    <h2 *ngIf="displayName; else nameElseBlock">
+      Codevolution
+    </h2>
+    <ng-template #nameElseBlock>
+      <h2>
+        Name is hidden
+      </h2>
+    </ng-template>
+
+    <div *ngIf="displayName; then thenBlock; else elseBlock"></div>
+
+    <ng-template #thenBlock>
+      <h2>Codevolution</h2>
+    </ng-template>
+
+    <ng-template #elseBlock>
+      <h2>Hidden</h2>    
+    </ng-template>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
+  public displayName = false;
   constructor() { }
 
   ngOnInit() {
