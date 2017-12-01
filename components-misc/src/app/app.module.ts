ba826200f93c757cff8ef9ce288ec0eb173c09f7
Vishwas <gopinav5@gmail.com>
2017-12-01 17:41:57 +0530

    Initial Commit

---
+++ components-misc/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ components-misc/src/app/test/test.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <h2>
+      Codevolution
+    </h2>
+  `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
