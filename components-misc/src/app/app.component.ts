5ae7fb71970d2ed97ca77185d4c523aaae5a0b6d
Vishwas <gopinav5@gmail.com>
2017-12-02 14:53:56 +0530

    Component Interaction

--- components-misc/src/app/app.component.ts
+++ components-misc/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  public name = "Vishwas";
+  public message = "";
+
 }
--- components-misc/src/app/test/test.component.ts
+++ components-misc/src/app/test/test.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-test',
   template: `
     <h2>
-      Codevolution
+      {{"Hello " + name}}
     </h2>
+    <button (click)=fireEvent()>Send Event</button>
   `,
   styles: []
 })
 export class TestComponent implements OnInit {
 
+  @Input('parentData') public name;
+  @Output() public childEvent = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  fireEvent(){
+    this.childEvent.emit('Hey Codevolution');
   }
 
 }
