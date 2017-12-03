54d559b03ea493e3f5218635262349b01e6978c5
Vishwas <gopinav5@gmail.com>
2017-12-03 18:38:41 +0530

    Commit pipes

--- components-misc/src/app/test/test.component.ts
+++ components-misc/src/app/test/test.component.ts
@@ -7,6 +7,35 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
       {{"Hello " + name}}
     </h2>
     <button (click)=fireEvent()>Send Event</button>
+
+    <h2>{{"Codevolution" | lowercase}}</h2>
+    <h2>{{"Codevolution" | uppercase}}</h2>
+    <h2>{{"Welcome to codevolution" | titlecase}}</h2>
+    <h2>{{"Codevolution" | slice:3:6}}</h2>
+    <h2>{{person | json}}</h2>
+
+    <h2>{{5.678 | number:'1.2-3'}}</h2>
+    <h2>{{5.678 | number:'3.4-5'}}</h2>
+    <h2>{{5.678 | number:'3.1-2'}}</h2>
+    
+    
+    <h2>{{0.25 | percent}}</h2>
+    <h2>{{0.25 | currency}}</h2>
+    <h2>{{0.25 | currency:'GBP'}}</h2>
+    <h2>{{0.25 | currency:'GBP':'code'}}</h2>
+    
+    <h2>{{date}}</h2>
+    <h2>{{date | date:'short'}}</h2>
+    <h2>{{date | date:'shortDate'}}</h2>
+    <h2>{{date | date:'shortTime'}}</h2>
+    
+    
+    
+    
+    
+    
+    
+    
   `,
   styles: []
 })
@@ -15,6 +44,12 @@ export class TestComponent implements OnInit {
   @Input('parentData') public name;
   @Output() public childEvent = new EventEmitter<string>();
 
+  public person = {
+    "firstName": "John",
+    "lastName": "Doe"
+  }
+
+  public date = new Date();
   constructor() { }
 
   ngOnInit() {
