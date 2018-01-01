7c0065554058174789e264cd639dc6101d32bd89
Vishwas <gopinav5@gmail.com>
2018-01-01 17:51:40 +0530

    Relative Navigation

--- routing-demo/src/app/department-detail/department-detail.component.ts
+++ routing-demo/src/app/department-detail/department-detail.component.ts
@@ -6,30 +6,38 @@ import { ActivatedRoute, Router, ParamMap } from '@angular/router';
     <h3>You selected department with id = {{departmentId}}</h3>
     <a (click)="goPrevious()">Previous</a>
     <a (click)="goNext()">Next</a>
+    <div>
+      <button (click)="gotoDepartments()">Back</button>
+    </div>
   `,
   styles: []
 })
 export class DepartmentDetailComponent implements OnInit {
 
   public departmentId;
-  constructor(private route: ActivatedRoute, private router: Router){}
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     //let id = parseInt(this.route.snapshot.paramMap.get('id'));
     this.route.paramMap.subscribe((params: ParamMap) => {
       let id = parseInt(params.get('id'));
       this.departmentId = id;
-    
-    } );
+
+    });
   }
 
-  goPrevious(){
+  goPrevious() {
     let previousId = this.departmentId - 1;
     this.router.navigate(['/departments', previousId]);
   }
-  goNext(){
-   let nextId = this.departmentId + 1;    
-   this.router.navigate(['/departments', nextId]);
+  goNext() {
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
   }
 
+  gotoDepartments() {
+    let selectedId = this.departmentId ? this.departmentId : null;
+    //this.router.navigate(['/departments', {id: selectedId}]);   
+    this.router.navigate(['../', { id: selectedId }], { relativeTo: this.route });
+  }
 }
--- routing-demo/src/app/department-list/department-list.component.ts
+++ routing-demo/src/app/department-list/department-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router} from '@angular/router';
+import { Router, ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-department-list',
@@ -8,7 +8,7 @@ import { Router} from '@angular/router';
       Department List
     </h3>
     <ul class="items">
-      <li *ngFor="let department of departments" (click)="onSelect(department)">
+      <li *ngFor="let department of departments" [class.selected]="isSelected(department)" (click)="onSelect(department)">
         <span class="badge">{{department.id}}</span> {{department.name}}
       </li>
   </ul>
@@ -16,7 +16,8 @@ import { Router} from '@angular/router';
   styles: []
 })
 export class DepartmentListComponent implements OnInit {
-
+  
+  public selectedId;
   departments = [
     {"id": 1, "name": "Angular"},
     {"id": 2, "name": "Node"},
@@ -24,13 +25,20 @@ export class DepartmentListComponent implements OnInit {
     {"id": 4, "name": "Ruby"},
     {"id": 5, "name": "Bootstrap"}
   ]
-  constructor(private router: Router) { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.selectedId = id;
+    
+    } );
   }
 
   onSelect(department) {
-    this.router.navigate(['/departments', department.id]);
-  
+    //this.router.navigate(['/departments', department.id]);
+     this.router.navigate([department.id], { relativeTo: this.route });
   }
+
+  isSelected(department) { return department.id === this.selectedId; }
 }
